300918edd1c00a7771887e0ae4ad53b27ae12d61
kimseulbi <ksb5166@gmail.com>
2018-12-04 12:11:23 +0900

    [itemList] itemList 서버로 부터 목록 받아오기

--- src/components/ItemList/ItemListView.js
+++ src/components/ItemList/ItemListView.js
@@ -3,17 +3,32 @@ import { Link } from 'react-router-dom';
 
 export default class ItemListView extends Component {
   static defaultProps = {
-    products: [],
+    products: [
+      {
+        // company: '집반찬연구소',
+        // discount_rate: 0,
+        // item_name: '굴생채무침(2~3인분) 220g',
+        // list_thumbnail: '',
+        // origin_price: 9800,
+        // pk: 1,
+        // sale_price: 9800,
+      },
+    ],
   };
-
   render() {
     const { products } = this.props;
+    console.log(products);
     return (
       <div>
         {products.map(p => (
           <div key={p.pk}>
-            <img src={p.photo} alt={p.main_category} />
-            <Link to={`/categories/${p.itemId}`}>{p.item_name}</Link>
+            <img src={p.list_thumbnail} alt={p.item_name} />
+            <span>{p.discount_rate}</span>
+            <Link to={`/product/${p.id}`}>
+              [{p.company}]{p.item_name}
+            </Link>
+            <span>{p.origin_price}</span>
+            <span>{p.sale_price}</span>
           </div>
         ))}
       </div>
--- src/containers/ItemList.js
+++ src/containers/ItemList.js
@@ -13,23 +13,11 @@ export default class ItemList extends Component {
   }
 
   async componentDidMount() {
-    const { pk } = this.props;
-    const { data: products } = await api.get('/api/categories/', {
-      params: {
-        pk,
-      },
+    //   const { pk } = this.props;
+      const params = new URLSearchParams(this.props.location.search);
+      const { data: products } = await api.get('/api/categories/', {
+      params
     });
-    const arr = [];
-    let category = null;
-    products.forEach(p => {
-      if (category !== p.main_category) {
-        console.log(p.main_category);
-
-        category = p.main_category;
-        arr.push(p.photo);
-      }
-    });
-    console.log(arr);
     this.setState({
       products,
       loading: false,
@@ -39,10 +27,13 @@ export default class ItemList extends Component {
     const { products, loading } = this.state;
     console.log('products', products);
     const itemList = products.map(p => ({
+      company: p.company,
+      discount_rate: p.discount_rate,
+      item_name: p.item_name,
+      list_thumbnail: p.list_thumbnail,
+      origin_price: p.origin_price,
       pk: p.pk,
-      main_category: p.main_category,
-      photo: p.photo,
-      sub_category: p.sub_category,
+      sale_price: p.sale_price,
     }));
     return <ItemListView products={itemList} />;
   }
