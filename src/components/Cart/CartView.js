1f890f87c945295354a471c6444893198bcd9486
Kim Minyeob <stankim0525@gmail.com>
2018-12-09 01:32:33 +0900

    cart modal test

--- src/components/Cart/CartView.js
+++ src/components/Cart/CartView.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import './CartView.scss';
 import { TableBody } from 'semantic-ui-react';
 
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 export default class CartView extends Component {
-  static defaultProps = {
-    products: [],
-  };
+  // static defaultProps = {
+  //   products: [],
+  // };
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  }
+
   render() {
     return (
       <div className="Cart">
@@ -52,7 +69,24 @@ export default class CartView extends Component {
         </div>
         <div className="Cart__button">
           <button className="keep-shopping">계속 쇼핑</button>
-          <button className="choose-date">희망 배송일 선택하기</button>
+          <button className="choose-date" color="danger" onClick={this.toggle}>
+            희망 배송일 선택하기
+          </button>
+          <Modal isOpen={this.state.modal} toggle={this.toggle}>
+            <ModalHeader>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary">주문하기</Button>
+            </ModalFooter>
+          </Modal>
         </div>
       </div>
     );
