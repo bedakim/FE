8a01110199dc2884d7321c69ab08a232271781e1
kimseulbi <ksb5166@gmail.com>
2018-12-09 02:10:24 +0900

    b2bBanner 마크업 수정

--- src/components/ItemList/ItemB2bBanner.js
+++ src/components/ItemList/ItemB2bBanner.js
@@ -5,10 +5,9 @@ export default class ItemVisualBanner extends Component {
     return (
       <div className="ItemList__b2b-banner">
         <h2>단체주문이 필요하세요?</h2>
-        <a href="#">온라인 주문</a>
+        <span>온라인 주문</span>
         <p>
-          "전화문의는 "<strong>Tel. 1600-1362</strong>
-          "로 연락주세요."
+          전화문의는 <strong>Tel. 1600-1362</strong>로 연락주세요.
         </p>
       </div>
     );
