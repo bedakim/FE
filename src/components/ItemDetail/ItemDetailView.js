663215db591715e04cfff12c950ab74acea09fd8
kimseulbi <ksb5166@gmail.com>
2018-12-11 01:46:33 +0900

    [FEAT]_#4 [상품목록페이지] 상품목록에서 아이템 장바구니로 보내기 상위 상품정보 레이아웃 디자인, 옵션 및 버튼 추가 -> component 분할 필요

--- src/components/ItemDetail/ItemDetailView.js
+++ src/components/ItemDetail/ItemDetailView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import withLoading from '../../hoc/withLoading';
 import './ItemDetail.scss';
-import DetailInfoView from './DetailInfoView';
+import DetailContentView from './DetailContentView';
 
 class ItemDetailView extends Component {
   constructor(props) {
@@ -11,7 +11,18 @@ class ItemDetailView extends Component {
     this.state = {
       imageTypeT: [],
       imageTypeD: [],
+      quantity: 1,
     };
+  }
+  async componentDidMount() {
+    this.setState({
+      quantity: 1,
+    });
+  }
+  handleQuantiyChange(e) {
+    this.setState({
+      quantity: parseInt(e.target.value),
+    });
   }
 
   handleImageChange() {}
@@ -29,6 +40,8 @@ class ItemDetailView extends Component {
     } = this.props;
     const imageTypeT = itemimage_set.filter(i => i.photo_type === 'T');
     const imageTypeD = itemimage_set.filter(i => i.photo_type === 'D');
+    const { quantity } = this.state;
+    const totalPrice = sale_price * quantity;
     return (
       <div className="ItemDetail">
         <div className="ItemDetail__content">
@@ -36,6 +49,7 @@ class ItemDetailView extends Component {
           <div className="ItemDetail__detail--top">
             <div className="detail--top-image">
               <div className="image-top">
+                <img src="" alt="" />
                 <div>
                   {discount_rate === 0 ? (
                     ''
@@ -51,13 +65,14 @@ class ItemDetailView extends Component {
                   const p = imageTypeT[index];
                   return imageTypeT[index] ? (
                     <a
+                      key={index}
                       className="top-thumb"
                       onClick={() => this.handleImageChange}
                     >
                       <img src={p.photo} alt={p.item_image_pk} />
                     </a>
                   ) : (
-                    <a className="top-thumb" />
+                    <a className="top-thumb" key={index} />
                   );
                 })}
               </div>
@@ -66,21 +81,27 @@ class ItemDetailView extends Component {
               {discount_rate === 0 ? (
                 ''
               ) : (
-                <div className="badge-event">
+                <div className="badge badge-event">
                   <span>이벤트특가</span>
                 </div>
               )}
-              <div className="desc-item_name">
+              <h1 className="desc-item_name">
                 [{company}]{item_name}
-              </div>
-              <div className="desc-bt_txt">{description.added_words}</div>
+              </h1>
+              <p className="desc-bt_txt">{description.added_words}</p>
               <dl className="desc-info">
                 <dt>적립금</dt>
-                <dd>{description.point}</dd>
+                <dd>{`${description.point}원`}</dd>
                 <dt>배송타입</dt>
                 <dd>{description.delivery_type}</dd>
                 <dt>수령요일</dt>
                 <dd>{description.receive_day}</dd>
+                <dt>정기배송</dt>
+                {description.regular_delivery !== false ? (
+                  <dd>가능</dd>
+                ) : (
+                  <dd>불가</dd>
+                )}
                 <dt className="readable-hidden">가격</dt>
                 <dd className="desc_price">
                   {origin_price === sale_price ? (
@@ -91,15 +112,69 @@ class ItemDetailView extends Component {
                     </strike>
                   )}
                   <strong className="slae-price">
-                    {sale_price.toLocaleString() + '원'}
+                    {sale_price.toLocaleString()}
+                    <span>원</span>
                   </strong>
                 </dd>
               </dl>
-              <div className="desc-option-calc" />
+              <div className="desc-option-calc">
+                <form>
+                  <fieldset>
+                    <legend>옵션별 상품 금액 계산 </legend>
+                    <div className="option-only-quantity">
+                      <dl>
+                        <dt>수량</dt>
+                        <dd>
+                          <div className="item-account">
+                            <input
+                              type="number"
+                              value={quantity}
+                              onChange={e => this.handleQuantiyChange(e)}
+                              min="1"
+                              max="10"
+                            />
+                            <span>
+                              <a
+                                title="수량 더하기"
+                                className="up"
+                                onClick={() =>
+                                  this.setState({
+                                    quantity: this.state.quantity + 1,
+                                  })
+                                }
+                              >
+                                +
+                              </a>
+                              <a
+                                title="수량 빼기"
+                                className="down"
+                                onClick={() =>
+                                  this.setState({
+                                    quantity: this.state.quantity - 1,
+                                  })
+                                }
+                              >
+                                -
+                              </a>
+                            </span>
+                          </div>
+                        </dd>
+                      </dl>
+                    </div>
+                    <p className="calculated-price">
+                      <span className="price-label">총 상품금액</span>
+                      <strong className="price-number">
+                        {totalPrice.toLocaleString()}
+                      </strong>
+                      <span>원</span>
+                    </p>
+                    <button className="btn-mint btn-into-cart">담기</button>
+                  </fieldset>
+                </form>
+              </div>
             </div>
           </div>
-          <div className="ItemDetail__detail--list-box" />
-          <DetailInfoView itemimage_set={itemimage_set} />
+          <DetailContentView itemimage_set={itemimage_set} />
         </div>
       </div>
     );
